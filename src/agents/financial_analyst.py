b997c56580cde78d5cba4d8a2c329b10856cc0d6
Oweys Momenzada <oweiss123@protonmail.com>
2024-12-07 21:11:19 +0100

    add docs

--- src/agents/financial_analyst.py
+++ src/agents/financial_analyst.py
@@ -19,6 +19,14 @@ class FinancialAnalystAgent:
         self.news_fetcher_obj = news_fetcher.NewsFetcher(num_articles=5)
 
     def generate_financial_evaluation_on_bing_search_engine(self, ticker):
+        """ Generate a financial evaluation on a stock based on Bing search engine results.
+
+        Args:
+            ticker (str): The stock ticker to evaluate.
+
+        Returns:    
+            str: The generated financial evaluation.
+        """
         company_name = self.TICKER_OVERVIEW_DB[ticker]
         websearch_results = self.news_fetcher_obj.fetch_websearch_results_on_stock(ticker=ticker)
         context = "\n".join(websearch_results)
@@ -45,9 +53,14 @@ Be concise and ensure your analysis is focused, actionable, and cautious of risk
 
         return completion.choices[0].message
     
-    def generate_financial_evaluation_on_general_news(self, ticker):
+    def generate_financial_evaluation_on_general_news(self):
+        """ Generate a financial evaluation based on general news.
+
+        Returns:
+            str: The generated financial evaluation.
+        """
         # update the list first.
-        self.news_fetcher_obj.fetch_latest_news(ticker=ticker)
+        self.news_fetcher_obj.fetch_latest_news()
         self.news_fetcher_obj.fetched_news_general
         contexts = "\n".join(self.news_fetcher_obj.fetched_news_general)
 
@@ -59,7 +72,7 @@ Be concise and ensure your analysis is focused, actionable, and cautious of risk
             combined_context += f"Content: {item['content']}\n\n"
 
         
-        instruction = f"""You are a financial analyst tasked with evaluating a block of financial news, each item provided with a title, publisher, publishing date, and content. Analyze the news collectively, prioritizing the most recent, significant, and impactful stories.
+        instruction = f"""You are a financial analyst tasked with evaluating a block of financial news, each item provided with a title, publisher, publishing date, and content. Analyze the news collectively, prioritizing the most recent, significant, and impactful stories. You can consider your own expertise about historical events for your analysis.
 
 For the news block as a whole:
 1. **Recency Filter**: Assess the collective recency of the news and identify the most relevant time-sensitive items.
@@ -75,6 +88,51 @@ Your response should include:
 Focus on clarity, brevity, and actionable insights while filtering out irrelevant or outdated information."
 """
 
+        completion = self.client.chat.completions.create(
+            model="gpt-4o",
+            messages=[
+                {"role": "system", "content": instruction},
+                {
+                    "role": "user",
+                    "content": combined_context
+                }
+            ]
+        )
+
+        return completion.choices[0].message
+    
+    def generate_financial_evaluation_on_stock_news(self, ticker):
+        """ Generate a financial evaluation based on stock news.
+
+        Args:
+            ticker (str): The stock ticker to evaluate.
+
+        Returns:
+            str: The generated financial evaluation.
+        """
+        # update the list first.
+        company_name = self.TICKER_OVERVIEW_DB[ticker]
+        self.news_fetcher_obj.fetch_news_about_stock(ticker=ticker)
+        self.news_fetcher_obj.fetched_news_general
+        contexts = "\n".join(self.news_fetcher_obj.fetched_news_general)
+
+        combined_context = ""
+        for item in contexts:
+            combined_context += f"Newstitle: {item['title']}\n"
+            combined_context += f"Publisher: {item['publisher']}\n"
+            combined_context += f"Published: {item['published']}\n"
+            combined_context += f"Content: {item['content']}\n\n"
+
+        
+        instruction = f"""You are a financial analyst evaluating the {company_name} ({ticker}) stock for potential day trading with years of experience. Analyze the provided context, which includes recent news, price movements, analyst ratings, and other financial data as well as potential historical events based on your knowlededge. Identify the most relevant and actionable information while considering recency and relevancy of the given articles.
+
+Provide:
+1. A **summary** of the key information influencing short-term price movements.
+2. Your **opinion** on whether to pursue day trading this stock today, with reasoning.
+3. Specific **guidance** on how to proceed, including strategies or conditions to watch for.
+
+Be concise and ensure your analysis is focused, actionable, and cautious of risks.
+"""
         completion = self.client.chat.completions.create(
             model="gpt-4o",
             messages=[
